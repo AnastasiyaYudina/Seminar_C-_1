fa8f79c7b46e7d04b5d5fa9d60fe0a06a5a24985
Anastasiya <ydinaana@gmail.com>
2022-08-09 21:59:26 +0300

    ChangeDZ_1

---
+++ DZ_1/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 2. Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine ("Введите число 1:");
+// int namberA =Convert.ToInt32(Console.ReadLine ());
+// Console.WriteLine ("Введите число 2:");
+// int namberB =Convert.ToInt32(Console.ReadLine ());
+// int max = namberA;
+// int min = namberB;
+// if (namberA>max) max = namberA;
+// else min = namberA;
+// if (namberB>max) max = namberB;
+// else min = namberB;
+
+// Console.WriteLine ($"Mаксимальное число: {max} ");
+
+// Console.WriteLine ($"Минимальное число: {min}");
+
+
+
+// Задача 4. Напишите программу, которая принимает 
+// на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine ("Введите числа:");
+// int namberA =Convert.ToInt32(Console.ReadLine ());
+// int namberB =Convert.ToInt32(Console.ReadLine ());
+// int namberC =Convert.ToInt32(Console.ReadLine ());
+
+// int max = namberA;
+
+// if (namberA>max) max = namberA;
+// if (namberB>max) max = namberB;
+// if (namberC>max) max = namberC;
+
+// Console.WriteLine ($"Mаксимальное число: {max} ");
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine ("Введите число:");
+// int namber =Convert.ToInt32(Console.ReadLine ());
+// if (namber%2==0) 
+// {
+//     Console.WriteLine("Четное");
+// }
+// else
+// {
+//     Console.WriteLine("Нечетное");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine ("Введите число:");
+// int N =Convert.ToInt32(Console.ReadLine ());
+// int count = 1;
+// while (count <=N)
+// {
+//     { if (count%2==0)
+//     Console.WriteLine (count);
+//     }
+//     count++;
+// }
