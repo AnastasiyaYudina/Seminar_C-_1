955c5100720790f5348c10f0dccda91196e6056c
Anastasiya <ydinaana@gmail.com>
2022-08-09 22:18:46 +0300

    ChangeDZ_2

---
+++ DZ_2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая принимает на вход 
+//  трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 
+// Console.WriteLine ("Введите трехзначное число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int c = (a/10)%10;
+// Console.WriteLine (c);
+
+
+// Напишите программу, которая выводит третью 
+// цифру заданного числа или сообщает, что третьей цифры нет.
+// Console.WriteLine ("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int digit3 = number%10;
+
+// if (number>99)
+// {
+//     Console.WriteLine(digit3);
+// }
+// else 
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+
+// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным
+// Console.WriteLine ("Введите цифру обозначающую день недели [1,7]:");
+// int digit = Convert.ToInt32 (Console.ReadLine());
+
+// if (digit==6)
+// {
+//     Console.WriteLine("Выходной день");
+// }
+// else if (digit==7 ) 
+// {
+//     Console.WriteLine("Выходной день");
+// }
+// else if (digit<=5 && digit>=1)
+// {
+//     Console.WriteLine("Рабочий день");
+// }
+// else Console.WriteLine ("Цифра не обозначает день недели");
+
+
