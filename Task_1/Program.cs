4d0309a81f72b5c0ee24b9399e7767453a31cf6e
Anastasiya <ydinaana@gmail.com>
2022-08-27 20:33:40 +0300

    ChangeTask5

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -32,75 +32,75 @@
 
 // Запишите программу, котррая будет выдава 
 // название дня недели по заданному номеру
-Console.Clear ();
-Console.WriteLine ("Введите число");
-int day = Convert.ToInt32(Console.ReadLine());
-if (day<1||day>7)
-{
- Console.WriteLine ("Не верное число");
-}
-else
-{
-    if (day==1)
-    {
-        Console.WriteLine("Понедельник");
-    }
-    if (day==2)
-    {
-        Console.WriteLine("Вторник");
-       
-    }
-    if (day==3)
-    {
-        Console.WriteLine("Среда");
-    }
-}
-
-
-
-
-// 4 Напишите программу, которая на вход принимает одно число (N), 
-//а на выходе показывает все целые числа в промежутке от -N до N.
-
-// выводим текст с запросом в терминал
-// Console.Write("Введите число: ");
-// присваиваем переменной N введенное число
-// int N = Convert.ToInt32(Console.ReadLine());
-// обяъвляем новую переменную, в которую сохраняем значение N с противоположным знаком
-// int B = -N;
-
-// условный цикл, который будет "работать" до тех пор, пока значение переменной B меньше либо равно значению N
-// while (B <= N)
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int day = Convert.ToInt32(Console.ReadLine());
+// if (day < 1 || day > 7)
 // {
-//     // выводим на экран значение B и пробел
-//     Console.Write(B+ " ");
-//     // увеличиваем значение B на единицу
-//     B++; // B = B + 1 аналогично B += 1 
+//     Console.WriteLine("Не верное число");
+// }
+// else
+// {
+//     if (day == 1)
+//     {
+//         Console.WriteLine("Понедельник");
+//     }
+//     if (day == 2)
+//     {
+//         Console.WriteLine("Вторник");
+
+//     }
+//     if (day == 3)
+//     {
+//         Console.WriteLine("Среда");
+//     }
 // }
 
-// 5
-// Console.Write("Введите трехзначное число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// if (num > 99 && num <= 999){
-//     int x = num % 10;
-//     Console.WriteLine(x);
-// }
-// else{
-//     Console.WriteLine("Введенное число не трехзначное");
-// }
-// или 
-// Console.WriteLine("enter the number");
+
+
+
+// // 4 Напишите программу, которая на вход принимает одно число (N), 
+// //а на выходе показывает все целые числа в промежутке от -N до N.
+
+// // выводим текст с запросом в терминал
+// // Console.Write("Введите число: ");
+// // присваиваем переменной N введенное число
+// // int N = Convert.ToInt32(Console.ReadLine());
+// // обяъвляем новую переменную, в которую сохраняем значение N с противоположным знаком
+// // int B = -N;
+
+// // условный цикл, который будет "работать" до тех пор, пока значение переменной B меньше либо равно значению N
+// // while (B <= N)
+// // {
+// //     // выводим на экран значение B и пробел
+// //     Console.Write(B+ " ");
+// //     // увеличиваем значение B на единицу
+// //     B++; // B = B + 1 аналогично B += 1 
+// // }
+
+// // 5
+// // Console.Write("Введите трехзначное число: ");
 // // int num = Convert.ToInt32(Console.ReadLine());
-// // Console.Write("result " + Math.Abs(num) % 10);
+// // if (num > 99 && num <= 999){
+// //     int x = num % 10;
+// //     Console.WriteLine(x);
+// // }
+// // else{
+// //     Console.WriteLine("Введенное число не трехзначное");
+// // }
+// // или 
+// // Console.WriteLine("enter the number");
+// // // int num = Convert.ToInt32(Console.ReadLine());
+// // // Console.Write("result " + Math.Abs(num) % 10);
 
 
-// Напишите программу, которая выводитт случайное число 
-// из отрезка (10,99) и показывае наибольшую цифру числа 
+// // Напишите программу, которая выводитт случайное число 
+// // из отрезка (10,99) и показывае наибольшую цифру числа 
 
-// 
+// // 
 
 
-// Напишите программу, которая выводит случайное трехзначное числои удаляет 
+// // Напишите программу, которая выводит случайное трехзначное числои удаляет 
 
 
 
@@ -320,7 +320,7 @@ else
 //         Console.WriteLine("x>0 y<0");
 //         break;
 //     }
-    
+
 //     default:
 //     {
 //         Console.WriteLine("Введена неправильная четверть");
@@ -502,3 +502,123 @@ else
 // {
 //     if GetRandomArray
 // }
+
+
+
+
+
+
+
+// Семинар 6
+
+
+
+
+
+
+//Напишите программу, которая перевернёт одномерный массив
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// Console.Clear();
+
+// int[] array = GetArray(10, 0, 10);
+// Console.WriteLine(String.Join(" ", array));
+
+// int[] reversArray=ReversArray2(array);
+// Console.WriteLine(String.Join(" ", reversArray));
+
+// ReversArray1(array);
+// Console.WriteLine(String.Join(" ", array));
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+// int[] res = new int[size];
+
+// for (int i = 0; i < size; i++)
+// {
+// res[i] = new Random().Next(minValue, maxValue + 1);
+// }
+// return res;
+// }
+
+// void ReversArray1(int[] inArray)
+// {
+// for (int i = 0; i < inArray.Length / 2; i++)
+// {
+// int k = inArray[i];
+// inArray[i] = inArray[inArray.Length - i - 1];
+// inArray[inArray.Length - i - 1] = k;
+// }
+// }
+
+// int[] ReversArray2(int[] inArray)
+// {
+// int[] result = new int[inArray.Length];
+// for (int i = 0; i < inArray.Length; i++)
+// {
+// result[i] = inArray[inArray.Length - 1 - i];
+
+// }
+// return result;
+// }
+
+// //Напишите программу, которая перевернёт одномерный массив
+// //(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+// Console.Clear();
+// int []array = GetArray(10,0,10);
+// Console.WriteLine (String.Join (" ", array));
+
+// int [] reversArray = ReversArray2(array);
+// Console.WriteLine (String.Join (" ", reversArray));
+
+// ReversArray1(array);
+// Console.WriteLine (String.Join (" ", array));
+
+// int []GetArray (int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
+//     for (int i=0; i<size; i++)
+//     {
+//     res [i]= new Random().Next (minValue, maxValue);
+//     }
+//     return res;
+// }
+
+// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+
+// Console.WriteLine("Введите первое число ");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число ");
+// int num2 = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите третье число ");
+// int num3 = int.Parse(Console.ReadLine()!);
+
+// if (num1 < num2 + num3 && num2<num1+num3 && num3<num2+num1)
+// {
+//     Console.WriteLine ("Будет существовать");
+// }
+// else 
+// {
+//     Console.WriteLine ("Не будет существовать");
+// }
+
+
+// Решение через массив 
+
+Console.Clear();
+Console.WriteLine ("Введите стороны треугольника через пробел");
+
+string [] st = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+if(IsTreangle(int.Parse(st[0]),int.Parse(st[1]),int.Parse(st[2])))
+{
+    Console.WriteLine ("Да");
+}
+else
+{
+    Console.WriteLine ("Нет");
+}
+
+bool IsTreangle (int a, int b, int c)
+{
+return ((a+b)>c && (a+c)>b && (b+c)>a);
+}
